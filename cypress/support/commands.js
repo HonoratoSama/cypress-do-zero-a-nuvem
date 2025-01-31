b6ed3d364122263e71f22fa32bade218ce796a86
Honorato <jaimehonoratos@gmail.com>
2025-01-30 22:07:00 -0300

    retiradas as funções Delay do arquivo commands2.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,18 +1,18 @@
 Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
 
-    cy.get('#firstName').type('Jaime',{delay:150})
+    cy.get('#firstName').type('Jaime')
     cy.get('#firstName').should('have.value','Jaime')
 
-    cy.get('#lastName').type('Honorato',{delay:150})
+    cy.get('#lastName').type('Honorato')
     cy.get('#lastName').should('have.value','Honorato')
 
-    cy.get('#email').type('jaime@gmail.com',{delay:150})
+    cy.get('#email').type('jaime@gmail.com')
     cy.get('#email').should('have.value','jaime@gmail.com')
  
-    cy.get('#phone').type('11964200039', {delay:100})
+    cy.get('#phone').type('11964200039')
     cy.get('#phone').should('have.value','11964200039')
 
-    cy.get('#open-text-area').type('teste de comandos personalizados',{delay:150})
+    cy.get('#open-text-area').type('teste de comandos personalizados')
     cy.get('#open-text-area').should('have.value','teste de comandos personalizados')    
 
     cy.get('button[type="submit"]').click()
